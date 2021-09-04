71211137817dcaef9edd6c9ae92e8e8af733840e
Jesse Rosenberger <git@jro.cc>
2021-09-04 07:36:53 +0000

    test fixes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,8 +4,9 @@ import * as path from 'path'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['GITHUB_REPOSITORY'] = 'apollographql/rover'
-  // process.env['INPUT_ORGANIZATION'] = 'org'
+  process.env['INPUT_API_URL'] =
+    'https://webhook.site/f636ed30-e72e-4996-881e-f76efcfe67c3'
+  process.env['INPUT_BEARER_TOKEN'] = '123abc'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
